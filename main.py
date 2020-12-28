cc8f0602562825c99f4fcc90749ff660462ebcca
Noah Garrett <67662284+xsychgames@users.noreply.github.com>
2020-12-28 11:54:27 -0500

    Updated help menu

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ REGION = 'na1'
 LOL_WATCHER = LolWatcher(API_KEY)
 
 client = commands.Bot(command_prefix= '#')
+client.remove_command('help')
 token = 'NzkxMzM2MTk0MjMwODQ1NDkw.X-NrQw.tOvPuEh5k6mW6ltEgTsiBPaOhvA'
 
 ## Cog Setup ##
@@ -34,6 +35,69 @@ async def get_champ_list():
     current_champ_list = LOL_WATCHER.data_dragon.champions(champ_version)
     return current_champ_list
 
+## Help Command ##
+@client.group(invoke_without_command=True)
+async def help(ctx):
+    em = discord.Embed(
+        title='ScuttleBot v1.0.0',
+        description='Use #help <command> for extended information on a command',
+        color=discord.Color.orange()
+    )
+    em.add_field(name='Champion Info', value='`champion\n` `stats`', inline=True)
+    em.add_field(name='Player Info', value='`rank`', inline=True)
+    em.add_field(name='Champion Selecter', value='`rchamp`', inline=True)
+
+    em.add_field(name='\u200b', value='\u200b', inline=False)
+
+    em.add_field(name='Invite Me!', value='[Invite](https://discord.com/oauth2/authorize?client_id=791336194230845490&scope=bot&permissions=347201)',
+                 inline=True)
+    em.add_field(name='Join My Support Server', value='[The River](https://discord.gg/VMwphqfmQk)', inline=True)
+
+    await ctx.send(embed=em)
+
+@help.command()
+async def champion(ctx):
+    em = discord.Embed(
+        title='#champion',
+        description="Displays a champion's win-rate, ban-rate, etc. for the current patch\n"
+                    "Displays a champion's optimal runes for the current patch",
+        color=discord.Color.orange()
+    )
+    em.add_field(name='Syntax', value='`#champion <championName> <role>`')
+    em.add_field(name='Note', value='`<championName> Please input name w/o spaces`', inline=False)
+    await ctx.send(embed=em)
+
+@help.command()
+async def stats(ctx):
+    em = discord.Embed(
+        title='#stats',
+        description="Displays a champion's in depth statistics",
+        color=discord.Color.orange()
+    )
+    em.add_field(name='Syntax', value='`#stats <championName>`')
+    em.add_field(name='Note', value='`<championName> Please input name w/o spaces`', inline=False)
+    await ctx.send(embed=em)
+
+@help.command()
+async def rank(ctx):
+    em = discord.Embed(
+        title='#rank',
+        description="Displays a provided summoner's ranked statistics",
+        color=discord.Color.orange()
+    )
+    em.add_field(name='Syntax', value='`#rank <summonerName>`')
+    em.add_field(name='Note', value='`<summonerName> Please input name w/o spaces`', inline=False)
+    await ctx.send(embed=em)
+
+@help.command()
+async def rchamp(ctx):
+    em = discord.Embed(
+        title='#rchamp',
+        description="Provides a random champion + a random role",
+        color=discord.Color.orange()
+    )
+    em.add_field(name='Syntax', value='`#rchamp`')
+    await ctx.send(embed=em)
 
 ## Runs Bot ##
 client.run(token)
